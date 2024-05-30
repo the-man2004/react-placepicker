d383a0e7599447f5d42359d35e17467866542272
the-man2004 <heinrichhb111@gmail.com>
2024-05-30 15:39:06 -0800

    Added a useEffect hook in the Modal.jsx component.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,7 +1,7 @@
-import { useEffect, useRef } from "react";
+import { useRef, useEffect } from "react";
 import { createPortal } from "react-dom";
 
-function Modal({ open, children }) {
+function Modal({ open, children, onClose }) {
   const dialog = useRef();
 
   useEffect(() => {
@@ -10,10 +10,10 @@ function Modal({ open, children }) {
     } else {
       dialog.current.close();
     }
-  }, []);
+  }, [open]);
 
   return createPortal(
-    <dialog className="modal" ref={dialog} open={open}>
+    <dialog className="modal" ref={dialog} onClose={onClose}>
       {children}
     </dialog>,
     document.getElementById("modal")
